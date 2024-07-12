e0ed10016b820e645efbf17d759732b048360eb5
YuriBaena <yuribaenaT@hotmail.com>
2024-07-12 16:21:17 -0300

    Movimento perfeito dos Bispos

--- Pecas/bispos.py
+++ Pecas/bispos.py
@@ -5,24 +5,116 @@ class BispoP:
 
         self.posicao = [x, y]
 
-    def alternativas(self):
+    def alternativas(self, posicoes):
 
         self.possibilidades = []
 
-        # Diagonal Principal (x + y constantes)
-        for k in range(-7, 8):
-            possivel1 = [self.posicao[0] + k, self.posicao[1] + k]
-            if 0 <= possivel1[0] < 8 and 0 <= possivel1[1] < 8 and possivel1 != self.posicao:
-                self.possibilidades.append(possivel1)
+        linhas = [str(i) for i in range(8, 0, -1)]
+        colunas = [chr(i) for i in range(ord('a'), ord('h') + 1)]
 
-        # Diagonal Secundária (x - y constantes)
-        for k in range(-7, 8):
-            possivel2 = [self.posicao[0] + k, self.posicao[1] - k]
-            if 0 <= possivel2[0] < 8 and 0 <= possivel2[1] < 8 and possivel2 != self.posicao:
-                self.possibilidades.append(possivel2)
+        # Direita Cima
+        if self.posicao[1] != 7:
+            direita_cima = 8 - self.posicao[1] + 1
+        else:
+            direita_cima = 0
 
-        # Remover posição atual das possibilidades
-        self.possibilidades = [pos for pos in self.possibilidades if pos != self.posicao]
+        for k in range(1, direita_cima):
+
+            if 0 <= self.posicao[1] + k < 8 and 0 <= self.posicao[0] - k < 8:
+
+                prox = colunas[self.posicao[1] + k] + linhas[self.posicao[0] - k]
+                tem = posicoes[prox].ocupado
+
+                if tem:
+                    cor = posicoes[prox].peca.cor
+
+                possivel = [self.posicao[0] - k, self.posicao[1] + k]
+
+                if not tem or cor != self.cor:
+                    self.possibilidades.append(possivel)
+                else:
+                    break
+
+            else:
+                break
+
+        # Esquerda Cima
+        if self.posicao[1] != 0:
+            esquerda_cima = self.posicao[1] + 1
+        else:
+            esquerda_cima = 0
+
+        for k in range(1, esquerda_cima):
+
+            if 0 <= self.posicao[1] - k < 8 and 0 <= self.posicao[0] - k < 8:
+
+                prox = colunas[self.posicao[1] - k] + linhas[self.posicao[0] - k]
+                tem = posicoes[prox].ocupado
+
+                if tem:
+                    cor = posicoes[prox].peca.cor
+
+                possivel = [self.posicao[0] - k, self.posicao[1] - k]
+
+                if not tem or cor != self.cor:
+                    self.possibilidades.append(possivel)
+                else:
+                    break
+
+            else:
+                break
+
+        # Direita Baixo
+        if self.posicao[1] != 7:
+            direita_baixo = 8 - self.posicao[1] + 1
+        else:
+            direita_baixo = 0
+
+        for k in range(1, direita_baixo):
+
+            if 0 <= self.posicao[1] + k < 8 and 0 <= self.posicao[0] + k < 8:
+
+                prox = colunas[self.posicao[1] + k] + linhas[self.posicao[0] + k]
+                tem = posicoes[prox].ocupado
+
+                if tem:
+                    cor = posicoes[prox].peca.cor
+
+                possivel = [self.posicao[0] + k, self.posicao[1] + k]
+
+                if not tem or cor != self.cor:
+                    self.possibilidades.append(possivel)
+                else:
+                    break
+
+            else:
+                break
+
+        # Esquerda Baixo
+        if self.posicao[1] != 0:
+            esquerda_baixo = self.posicao[1] + 1
+        else:
+            esquerda_baixo = 0
+
+        for k in range(1, esquerda_baixo):
+
+            if 0 <= self.posicao[1] - k < 8 and 0 <= self.posicao[0] + k < 8:
+
+                prox = colunas[self.posicao[1] - k] + linhas[self.posicao[0] + k]
+                tem = posicoes[prox].ocupado
+
+                if tem:
+                    cor = posicoes[prox].peca.cor
+
+                possivel = [self.posicao[0] + k, self.posicao[1] - k]
+
+                if not tem or cor != self.cor:
+                    self.possibilidades.append(possivel)
+                else:
+                    break
+
+            else:
+                break
 
 
 class BispoB:
@@ -32,22 +124,115 @@ class BispoB:
 
         self.posicao = [x, y]
 
-    def alternativas(self):
+    def alternativas(self, posicoes):
+
         self.possibilidades = []
 
-        # Diagonal Principal (x + y constantes)
-        for k in range(-7, 8):
-            possivel1 = [self.posicao[0] + k, self.posicao[1] + k]
-            if 0 <= possivel1[0] < 8 and 0 <= possivel1[1] < 8 and possivel1 != self.posicao:
-                self.possibilidades.append(possivel1)
+        linhas = [str(i) for i in range(8, 0, -1)]
+        colunas = [chr(i) for i in range(ord('a'), ord('h') + 1)]
 
-        # Diagonal Secundária (x - y constantes)
-        for k in range(-7, 8):
-            possivel2 = [self.posicao[0] + k, self.posicao[1] - k]
-            if 0 <= possivel2[0] < 8 and 0 <= possivel2[1] < 8 and possivel2 != self.posicao:
-                self.possibilidades.append(possivel2)
+        # Direita Cima
+        if self.posicao[1] != 7:
+            direita_cima = 8 - self.posicao[1] + 1
+        else:
+            direita_cima = 0
 
-        # Remover posição atual das possibilidades
-        self.possibilidades = [pos for pos in self.possibilidades if pos != self.posicao]
+        for k in range(1, direita_cima):
+
+            if 0 <= self.posicao[1] + k < 8 and 0 <= self.posicao[0] - k < 8:
+
+                prox = colunas[self.posicao[1] + k] + linhas[self.posicao[0] - k]
+                tem = posicoes[prox].ocupado
+
+                if tem:
+                    cor = posicoes[prox].peca.cor
+
+                possivel = [self.posicao[0] - k, self.posicao[1] + k]
+
+                if not tem or cor != self.cor:
+                    self.possibilidades.append(possivel)
+                else:
+                    break
+
+            else:
+                break
+
+        # Esquerda Cima
+        if self.posicao[1] != 0:
+            esquerda_cima = self.posicao[1] + 1
+        else:
+            esquerda_cima = 0
+
+        for k in range(1, esquerda_cima):
+
+            if 0 <= self.posicao[1] - k < 8 and 0 <= self.posicao[0] - k < 8:
+
+                prox = colunas[self.posicao[1] - k] + linhas[self.posicao[0] - k]
+                tem = posicoes[prox].ocupado
+
+                if tem:
+                    cor = posicoes[prox].peca.cor
+
+                possivel = [self.posicao[0] - k, self.posicao[1] - k]
+
+                if not tem or cor != self.cor:
+                    self.possibilidades.append(possivel)
+                else:
+                    break
+
+            else:
+                break
 
 
+        # Direita Baixo
+        if self.posicao[1] != 7:
+            direita_baixo = 8 - self.posicao[1] + 1
+        else:
+            direita_baixo = 0
+
+        for k in range(1, direita_baixo):
+
+            if 0 <= self.posicao[1] + k < 8 and 0 <= self.posicao[0] + k < 8:
+
+                prox = colunas[self.posicao[1] + k] + linhas[self.posicao[0] + k]
+                tem = posicoes[prox].ocupado
+
+                if tem:
+                    cor = posicoes[prox].peca.cor
+
+                possivel = [self.posicao[0] + k, self.posicao[1] + k]
+
+                if not tem or cor != self.cor:
+                    self.possibilidades.append(possivel)
+                else:
+                    break
+
+            else:
+                break
+
+
+        # Esquerda Baixo
+        if self.posicao[1] != 0:
+            esquerda_baixo = self.posicao[1] + 1
+        else:
+            esquerda_baixo = 0
+
+        for k in range(1, esquerda_baixo):
+
+            if 0 <= self.posicao[1] - k < 8 and 0 <= self.posicao[0] + k < 8:
+
+                prox = colunas[self.posicao[1] - k] + linhas[self.posicao[0] + k]
+                tem = posicoes[prox].ocupado
+
+                if tem:
+                    cor = posicoes[prox].peca.cor
+
+                possivel = [self.posicao[0] + k, self.posicao[1] - k]
+
+                if not tem or cor != self.cor:
+                    self.possibilidades.append(possivel)
+                else:
+                    break
+
+            else:
+                break
--- Pecas/rainhas.py
+++ Pecas/rainhas.py
@@ -6,6 +6,7 @@ class RainhaP:
         self.posicao = [x, y]
 
     def alternativas(self, posicoes):
+
         self.possibilidades = []
 
         # Vertical e Horizontal
@@ -15,20 +16,18 @@ class RainhaP:
             if i != self.posicao[1]:  # horizontal (y fixo)
                 self.possibilidades.append([self.posicao[0], i])
 
-        # Diagonal Principal (x + y constantes)
+        # Diagonais
         for k in range(-7, 8):
+            # Diagonal Principal (x + y constantes)
             possivel1 = [self.posicao[0] + k, self.posicao[1] + k]
             if 0 <= possivel1[0] < 8 and 0 <= possivel1[1] < 8 and possivel1 != self.posicao:
                 self.possibilidades.append(possivel1)
 
-        # Diagonal Secundária (x - y constantes)
-        for k in range(-7, 8):
+            # Diagonal Secundária (x - y constantes)
             possivel2 = [self.posicao[0] + k, self.posicao[1] - k]
             if 0 <= possivel2[0] < 8 and 0 <= possivel2[1] < 8 and possivel2 != self.posicao:
                 self.possibilidades.append(possivel2)
 
-        # Remover posição atual das possibilidades
-        self.possibilidades = [pos for pos in self.possibilidades if pos != self.posicao]
 
 class RainhaB:
     def __init__(self, x, y):
@@ -42,23 +41,24 @@ class RainhaB:
 
         # Vertical e Horizontal
         for i in range(8):
+
             if i != self.posicao[0]:  # vertical (x fixo)
                 self.possibilidades.append([i, self.posicao[1]])
             if i != self.posicao[1]:  # horizontal (y fixo)
                 self.possibilidades.append([self.posicao[0], i])
 
-        # Diagonal Principal (x + y constantes)
+        # Diagonais
         for k in range(-7, 8):
+
+            # Diagonal Principal (x + y constantes)
             possivel1 = [self.posicao[0] + k, self.posicao[1] + k]
             if 0 <= possivel1[0] < 8 and 0 <= possivel1[1] < 8 and possivel1 != self.posicao:
                 self.possibilidades.append(possivel1)
 
-        # Diagonal Secundária (x - y constantes)
-        for k in range(-7, 8):
+            # Diagonal Secundária (x - y constantes)
             possivel2 = [self.posicao[0] + k, self.posicao[1] - k]
             if 0 <= possivel2[0] < 8 and 0 <= possivel2[1] < 8 and possivel2 != self.posicao:
                 self.possibilidades.append(possivel2)
 
         # Remover posição atual das possibilidades
         self.possibilidades = [pos for pos in self.possibilidades if pos != self.posicao]
-
