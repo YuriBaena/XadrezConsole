2b895ea452da3031213e29eec14da07b3b5d0658
YuriBaena <yuribaenaT@hotmail.com>
2024-07-13 15:29:59 -0300

    Promocao do peao

--- board.py
+++ board.py
@@ -69,73 +69,98 @@ class Tabuleiro:
 
     def mostra(self, movimentos=None):
         if movimentos is None:
-            movimentos = []
-        print()
+            movimentos = []  # Define uma lista vazia se nenhum movimento for especificado
+
+        print()  # Imprime uma linha em branco
+
         if len(movimentos) == 0:
+            # Se não houver movimentos a serem destacados, imprime o tabuleiro normalmente
             for linha in range(self.comprimento):
-                print(linhas[linha], end=" |   ")
+                print(linhas[linha], end=" |   ")  # Imprime a linha atual seguida de um separador
                 for coluna in range(self.altura):
-                    posicao = colunas[coluna] + linhas[linha]
+                    posicao = colunas[coluna] + linhas[linha]  # Calcula a posição atual no tabuleiro
                     if posicao in self.posicoes:
-                        print(f" {self.posicoes[posicao].img} ", end="")
+                        print(f" {self.posicoes[posicao].img} ", end="")  # Imprime a peça na posição, se existir
                     else:
-                        print(f" . ", end="")
-                print()
+                        print(f" . ", end="")  # Caso contrário, imprime um ponto para indicar posição vazia
+                print()  # Nova linha após imprimir cada linha do tabuleiro
 
-            print("  | _ _ _ _ _ _ _ _ _ _ _ _ _ ")
+            print("  | _ _ _ _ _ _ _ _ _ _ _ _ _ ")  # Linha separadora do tabuleiro
             print("      ", end="")
 
             for k in colunas:
-                print(f" {k} ", end="")
+                print(f" {k} ", end="")  # Imprime os rótulos das colunas na parte inferior do tabuleiro
 
-            print()
+            print()  # Nova linha após imprimir os rótulos das colunas
         else:
+            # Se houver movimentos a serem destacados, imprime o tabuleiro com esses movimentos destacados
             for linha in range(self.comprimento):
-                print(linhas[linha], end=" |   ")
+                print(linhas[linha], end=" |   ")  # Imprime a linha atual seguida de um separador
                 for coluna in range(self.altura):
-                    posicao = colunas[coluna] + linhas[linha]
+                    posicao = colunas[coluna] + linhas[linha]  # Calcula a posição atual no tabuleiro
                     if posicao in self.posicoes and posicao not in movimentos:
-                        print(f" {self.posicoes[posicao].img} ", end="")
+                        print(f" {self.posicoes[posicao].img} ",
+                              end="")  # Imprime a peça na posição, se não for um movimento
                     elif posicao in movimentos:
-                        print(f" # ", end="")
+                        print(f" # ", end="")  # Imprime '#' para indicar um movimento destacado
                     else:
-                        print(f" . ", end="")
-                print()
+                        print(f" . ", end="")  # Caso contrário, imprime um ponto para indicar posição vazia
+                print()  # Nova linha após imprimir cada linha do tabuleiro
 
-            print("  | _ _ _ _ _ _ _ _ _ _ _ _ _ ")
+            print("  | _ _ _ _ _ _ _ _ _ _ _ _ _ ")  # Linha separadora do tabuleiro
             print("      ", end="")
 
             for k in colunas:
-                print(f" {k} ", end="")
+                print(f" {k} ", end="")  # Imprime os rótulos das colunas na parte inferior do tabuleiro
 
-            print()
+            print()  # Nova linha após imprimir os rótulos das colunas
 
     def escolhe(self):
         while True:
             print()
+
+            # Escolhe peca para mover
             posicao = input("Informe a posição da peça \nque deseja usar (letra/num): ")
             if posicao in self.posicoes:
+
+                # Verifica se e peca valida
                 if self.vez == self.posicoes[posicao].peca.cor:
+
+                    # Cria lista de possiveis movimentos
                     movimentos = self.posicoes[posicao].coordena(self.posicoes)
+
+                    # Se tiver um ou mais movimentos entra aqui
                     if len(movimentos) != 0:
                         self.mostra(movimentos)
                         a = 0
+
+                        # Mostra todos os possiveis movimentos
                         for i, lugar in enumerate(movimentos):
                             print(f"[ {i + 1} ] para movimentar para {lugar}")
                             a = i
                         print(f"[ {a + 2} ] para escolher outra peça")
+
+                        # Pergunta qual movimento o jogador quer fazer
                         qual = int(input("Qual opção deseja: "))
                         if qual != a + 2:
                             pra_onde = movimentos[qual - 1]
                             peca = self.posicoes[posicao]
+
+                            # Movimenta peca escolhida
                             self.movimenta(peca, pra_onde)
+
+                            # Promove o peao se for necessario
+                            self.promove_peao()
+
+                            # Renova possiveis movimentos
                             self.renova_movimentos_possiveis()
-                            if self.procura_cheque():
-                                if self.vez == "P":
-                                    print(f"\n\n---O Branco está em xeque---\n\n")
-                                else:
-                                    print(f"\n\n---O Preto está em xeque---\n\n")
+
+                            # Verifica se algum rei esta em cheque
+                            self.procura_cheque()
+
                             break
+
+                    # Se nao tiver nenhum movimento
                     else:
                         print("Não existem movimentos possíveis para essa peça")
                 else:
@@ -223,6 +248,7 @@ class Tabuleiro:
                     self.movimentos_possiveis_branco[i.peca] = i.peca.possibilidades
 
     def procura_cheque(self):
+
         # Verifica se o rei Branco está em xeque
         for i in self.posicoes.values():
             if i.img == "♔":
@@ -231,7 +257,7 @@ class Tabuleiro:
         for i in self.movimentos_possiveis_preto.values():
             for posicao in i:
                 if posicao == posicao_do_rei_branco:
-                    return True
+                    print(f"\n!!!!!!!\n---O Branco está em xeque---\n!!!!!!!\n")
 
         # Verifica se o rei Preto está em xeque
         for i in self.posicoes.values():
@@ -241,9 +267,83 @@ class Tabuleiro:
         for i in self.movimentos_possiveis_branco.values():
             for posicao in i:
                 if posicao == posicao_do_rei_preto:
-                    return True
+                    print(f"\n!!!!!!!\n---O Preto está em xeque---\n!!!!!!!\n")
 
-        return False
+    def promove_peao(self):
+
+        troca = ["Torre", "Cavalo", "Bispo", "Rainha"]
+
+        for chave, valor in self.posicoes.items():
+
+            # Promove Preto
+            if valor.img == "♟" and valor.posicao[0] == 7:
+                for j, i in enumerate(troca):
+                    print(f"[ {j + 1} ] Deseja promover seu peao para {i}")
+
+                while True:
+                    numero = int(input("Qual opção deseja: "))
+
+                    if numero == 1:
+                        # Torre
+                        self.posicoes[chave] = Posicao(self.posicoes[chave].posicao[0], self.posicoes[chave].posicao[1],
+                                                       TorreP(self.posicoes[chave].posicao[0],
+                                                              self.posicoes[chave].posicao[1]))
+                        break
+                    elif numero == 2:
+                        # Cavalo
+                        self.posicoes[chave] = Posicao(self.posicoes[chave].posicao[0], self.posicoes[chave].posicao[1],
+                                                       CavaloP(self.posicoes[chave].posicao[0],
+                                                               self.posicoes[chave].posicao[1]))
+                        break
+                    elif numero == 3:
+                        # Bispo
+                        self.posicoes[chave] = Posicao(self.posicoes[chave].posicao[0], self.posicoes[chave].posicao[1],
+                                                       BispoP(self.posicoes[chave].posicao[0],
+                                                              self.posicoes[chave].posicao[1]))
+                        break
+                    elif numero == 4:
+                        # Rainha
+                        self.posicoes[chave] = Posicao(self.posicoes[chave].posicao[0], self.posicoes[chave].posicao[1],
+                                                       RainhaP(self.posicoes[chave].posicao[0],
+                                                               self.posicoes[chave].posicao[1]))
+                        break
+                    else:
+                        print("Opção invalida!")
+
+            # Promove Branco
+            if valor.img == "♙" and valor.posicao[0] == 0:
+                for j, i in enumerate(troca):
+                    print(f"[ {j} ] Deseja promover seu peao para {i}")
+
+                while True:
+                    numero = int(input("Qual opção deseja: "))
+
+                    if numero == 1:
+                        # Torre
+                        self.posicoes[chave] = Posicao(self.posicoes[chave].posicao[0], self.posicoes[chave].posicao[1],
+                                                       TorreB(self.posicoes[chave].posicao[0],
+                                                              self.posicoes[chave].posicao[1]))
+                        break
+                    elif numero == 2:
+                        # Cavalo
+                        self.posicoes[chave] = Posicao(self.posicoes[chave].posicao[0], self.posicoes[chave].posicao[1],
+                                                       CavaloB(self.posicoes[chave].posicao[0],
+                                                               self.posicoes[chave].posicao[1]))
+                        break
+                    elif numero == 3:
+                        # Bispo
+                        self.posicoes[chave] = Posicao(self.posicoes[chave].posicao[0], self.posicoes[chave].posicao[1],
+                                                       BispoB(self.posicoes[chave].posicao[0],
+                                                              self.posicoes[chave].posicao[1]))
+                        break
+                    elif numero == 4:
+                        # Rainha
+                        self.posicoes[chave] = Posicao(self.posicoes[chave].posicao[0], self.posicoes[chave].posicao[1],
+                                                       RainhaB(self.posicoes[chave].posicao[0],
+                                                               self.posicoes[chave].posicao[1]))
+                        break
+                    else:
+                        print("Opção invalida!")
 
 
 tabuleiro = Tabuleiro()
